2579eeb05d6ff4ba05b1e26d577a38966aef35ee
xiaoma <1223841837@qq.com>
2023-11-30 21:49:29 +0800

    添加四个路由

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import {
   createBrowserRouter,
+  NavLink,
+  Outlet,
   RouterProvider,
 } from "react-router-dom";
 import { ErrorPage } from './components/ErrorPage.tsx';
@@ -12,9 +14,38 @@ import { ErrorPage } from './components/ErrorPage.tsx';
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element:<Outlet/>,
     errorElement: <ErrorPage />,
+    children:[
+      {index:true,element:<div>空</div>},
+      {
+        path:'welcome',
+        element:<Outlet/>,
+        children:[
+          {index:true,element:<div>空</div>},
+          {path:'1',
+           element:
+            (<div>1111 <NavLink to="/welcome/2">下一页</NavLink></div>)
+          },
+          {path:'2',element:
+            (<div>2222 <NavLink to="/welcome/3">下一页</NavLink></div>)
+          },
+          {path:'3',
+           element:
+           (<div>3333 <NavLink to="/welcome/4">下一页</NavLink></div>)
+          },
+          {path:'4',element:
+            (<div>4444 <NavLink to="/xxx">开始记账</NavLink></div>)
+          },
+        ]
+      }
+    ]
+
+
+
+   
   },
+  
 
 ]);
 
