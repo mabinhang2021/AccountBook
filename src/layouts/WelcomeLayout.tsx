b3557206ca8ef8d3128cdb95e14ddec6d0ca8ce2
xiaoma <1223841837@qq.com>
2023-12-01 20:23:01 +0800

    优化路由动画，解决了map与WelcomeLayout生命周期不匹配的问题，保证第一次 加载welcome/1不会加载动画

--- src/layouts/WelcomeLayout.tsx
+++ src/layouts/WelcomeLayout.tsx
@@ -1,14 +1,14 @@
 import { animated, useTransition } from '@react-spring/web'
 import type { ReactNode } from 'react'
 import { useLocation, useOutlet } from 'react-router-dom'
-const map: Record<string, ReactNode> = {}
+import {useRef} from 'react'
 export const WelcomeLayout: React.FC = () => {
-  
+  const map = useRef<Record<string, ReactNode>>({}) 
   const location = useLocation() // 获取当前地址栏的信息
   // location.pathname === /welcome/1
   // location.pathname === /welcome/2
   const outlet = useOutlet()
-  map[location.pathname] = outlet
+  map.current[location.pathname] = outlet
   const transitions = useTransition(location.pathname, {
     // 进入状态
     from: { transform: 'translateX(100%)' },
@@ -18,11 +18,13 @@ export const WelcomeLayout: React.FC = () => {
     leave: { transform: 'translateX(-100%)' },
     config: { duration: 1000 }
   })
-  return transitions((style, pathname) => {
-    return <animated.div key={pathname} style={style}>
+  return transitions((style, pathname) => 
+     <animated.div key={pathname} style={style}>
       <div style={{ textAlign: 'center' }}>
-        {map[pathname]}
+        {map.current[pathname]}
       </div>
     </animated.div>
-  })
+  )
 }
+
+
